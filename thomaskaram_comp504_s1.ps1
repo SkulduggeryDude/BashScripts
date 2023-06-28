720aacd17939defbfd4307f4b6996eb481998285
Thomas Karam <131653869+SkulduggeryDude@users.noreply.github.com>
2023-06-29 08:41:48 +1200

    File upload

---
+++ thomaskaram_comp504_s1.ps1
@@ -0,0 +1,13 @@
+﻿cls
+#New-Item creates a new object, -ItemType creates either a directory or file and -Path selects where its made
+#-Force rewrites the file if its already been made, so this script can be run more than once.
+New-Item -Path "C:\" -Name "Test" -ItemType Directory -Force | Out-Null
+#Now the folder in C is made we can send the output of the Get-Date command there as a text file with the OutFile command
+Get-Date | Out-File C:\Test\File1.txt
+Get-Date | Out-File C:\Test\File2.txt
+Get-Date | Out-File C:\Test\File3.txt
+#To see the data, Get-Content reads the file at the chosen path and writes to console
+Get-Content -Path C:\Test\File1.txt
+Get-Content -Path C:\Test\File2.txt
+Get-Content -Path C:\Test\File3.txt
+echo "Thomas Karam 16448798"
---
+++ thomaskaram_comp504_s2.ps1
@@ -0,0 +1,31 @@
+﻿cls
+Write-Host "Thomas Karam 16448798"
+
+
+#New-Item creates a new object, -ItemType creates either a directory or file and -Path selects where its made
+#-Force rewrites the file if its already been made, so this script can be run more than once.
+#Out-Null prevents New-Item from printing to the console 
+New-Item -Path "C:\" -Name "Backup" -ItemType Directory -Force | Out-Null
+New-Item -Path "C:\Backup" -Name "Documents" -ItemType Directory -Force | Out-Null
+
+
+#Copy-Item copys a source folder/file to a destination. -Include only copies .docx files 
+#The \* at the end means all files in the directory are copied
+#-PassThru creates an output for the Copy-Item command and $copyCount stores this as a variable to be printed in the next line
+#I have used the $env:UserName variable to capture the current users name so that this program can be used on any computers file system
+$copyCount = Copy-Item -Path "C:\Users\$env:UserName\Documents\*" -Destination "C:\Backup\Documents\" -Include *.docx -PassThru
+#Write-Host prints to the console
+Write-Host Copied $copyCount.count files
+
+
+#As Remove-Item doesnt have -PassThru to make an output. Get-Children counts them before they are deleted and assigns them to a variable
+$deleteCount = Get-ChildItem -Path "C:\Backup\Documents\*" -Include *.tmp
+#Write-Host now prints this like we did for copying
+Write-Host Deleted $deleteCount.count files
+#Once it has been printed it removes them with the path saved in the variable and Remove-Item
+$deleteCount | Remove-Item
+
+
+#Compress-Archive is used to zip all fines in documents and put them in a zip file in backup. -Force will replace the existing file
+Compress-Archive -Path C:\Backup\Documents\* -DestinationPath "C:\Backup\Backup_Documents_ThomasKaram.zip" -Force
+Write-Host "Your backup has been zipped to C:\Backup"
